15cd016263b635cc31c6cde37742c6823c2ed185
jay <jaynarigara4@gmail.com>
2024-04-03 23:30:08 +0530

    insert image

--- src/Components/Casual/Casual.jsx
+++ src/Components/Casual/Casual.jsx
@@ -1,11 +1,149 @@
-import React from 'react'
+import React from "react";
 
 const Casual = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const CasualData = [
+    { 
+      title: "Casual T-Shirt",
+      id: 1,
+      image: "c1.jpg",
+      quantity: 1,
+      price: 19.99
+    },
+    { 
+      title: "Denim Jeans",
+      id: 2,
+      image: "c2.jpg",
+      quantity: 1,
+      price: 29.99
+    },
+    { 
+      title: "Hooded Sweatshirt",
+      id: 3,
+      image: "c3.jpg",
+      quantity: 1,
+      price: 39.99
+    },
+    { 
+      title: "Plaid Shirt",
+      id: 4,
+      image: "c4.jpg",
+      quantity: 1,
+      price: 24.99
+    },
+    { 
+      title: "Chino Pants",
+      id: 5,
+      image: "c5.jpg",
+      quantity: 1,
+      price: 34.99
+    },
+    { 
+      title: "Polo Shirt",
+      id: 6,
+      image: "c6.jpg",
+      quantity: 1,
+      price: 22.99
+    },
+    { 
+      title: "Cargo Shorts",
+      id: 7,
+      image: "c7.jpg",
+      quantity: 1,
+      price: 27.99
+    },
+    { 
+      title: "Casual Blazer",
+      id: 8,
+      image: "c8.jpg",
+      quantity: 1,
+      price: 49.99
+    },
+    { 
+      title: "Casual Dress",
+      id: 9,
+      image: "c9.jpg",
+      quantity: 1,
+      price: 39.99
+    },
+    { 
+      title: "Striped Sweater",
+      id: 10,
+      image: "c10.jpg",
+      quantity: 1,
+      price: 29.99
+    },
+    { 
+      title: "Casual Skirt",
+      id: 11,
+      image: "c11.jpg",
+      quantity: 1,
+      price: 26.99
+    },
+    { 
+      title: "Linen Shirt",
+      id: 12,
+      image: "c12.jpg",
+      quantity: 1,
+      price: 32.99
+    },
+    { 
+      title: "Printed T-Shirt",
+      id: 13,
+      image: "c13.jpg",
+      quantity: 1,
+      price: 21.99
+    },
+    { 
+      title: "Casual Jumpsuit",
+      id: 14,
+      image: "c14.jpg",
+      quantity: 1,
+      price: 44.99
+    },
+    { 
+      title: "Canvas Sneakers",
+      id: 15,
+      image: "c15.jpg",
+      quantity: 1,
+      price: 37.99
+    },
+    { 
+      title: "Cropped Jeans",
+      id: 16,
+      image: "c16.jpg",
+      quantity: 1,
+      price: 31.99
+    },
+    { 
+      title: "Casual Cardigan",
+      id: 17,
+      image: "c17.jpg",
+      quantity: 1,
+      price: 28.99
+    },
+    { 
+      title: "Athleisure Leggings",
+      id: 18,
+      image: "c18.jpg",
+      quantity: 1,
+      price: 23.99
+    },
+    { 
+      title: "Knit Pullover",
+      id: 19,
+      image: "c19.jpg",
+      quantity: 1,
+      price: 36.99
+    },
+    { 
+      title: "Casual Romper",
+      id: 20,
+      image: "c20.jpg",
+      quantity: 1,
+      price: 41.99
+    }
+  ];
+  return <div></div>;
+};
 
-export default Casual
+export default Casual;
